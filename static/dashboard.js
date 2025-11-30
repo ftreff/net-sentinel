3170f3644c13ca85df383b1c0500fd9033ae23ca
ftreff <ftreff@gmail.com>
2025-11-30 14:38:00 -0500

    Update dashboard.js

--- static/dashboard.js
+++ static/dashboard.js
@@ -281,18 +281,6 @@ function addTimeFilterControl() {
         <option value="Unknown">Unknown</option>
       </select>
 
-      <select id="frequencyFilter" onchange="onFilterChange()">
-        <option value="">All Frequencies</option>
-        <option value=">1">>1</option>
-        <option value=">5">>5</option>
-        <option value=">10">>10</option>
-        <option value=">25">>25</option>
-        <option value=">50">>50</option>
-        <option value=">100">>100</option>
-        <option value=">500">>500</option>
-        <option value=">1000">>1000</option>
-      </select>
-
       <select id="countryFilter" onchange="onFilterChange()">
         <option value="">All Countries</option>
       </select>
